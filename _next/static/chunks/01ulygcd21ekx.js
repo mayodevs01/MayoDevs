83c0e6d1d871a4decee1c58cfa73fdd18051d82c
mayodevs01 <Mayodevs01@gmail.com>
2026-04-26 14:07:48 +0530

    Updates

---
+++ _next/static/chunks/01ulygcd21ekx.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,60150,t=>{t.v(o=>Promise.all(["static/chunks/165dgqs6.w84p.js"].map(o=>t.l(o))).then(()=>o(88841)))}]);
